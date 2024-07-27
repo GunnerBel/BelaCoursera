f8876f6e65462590912951b9fa2e022cf4b95206
GunnerBel <m.bela@alustudent.com>
2024-07-27 11:01:25 +0200

    Looping array

---
+++ module4-solution/script.js
@@ -0,0 +1,15 @@
+// Array of names
+var names = ["James", "John", "Dennis", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Bela"];
+
+// Loop over the array of names
+for (var i = 0; i < names.length; i++) {
+  // Get the first letter of the current name
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  // Check if the first letter is 'j'
+  if (firstLetter === 'j') {
+    console.log("Goodbye " + names[i]);
+  } else {
+    console.log("Hello " + names[i]);
+  }
+}
